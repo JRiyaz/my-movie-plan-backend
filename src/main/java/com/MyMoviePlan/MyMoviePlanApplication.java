00c1213e9f2497bec954102886bf281e5058109d
Riyaz J <j.riyazu@gmail.com>
2021-05-18 13:21:47 +0530

    Create basic spring boot classes and test class.

---
+++ src/main/java/com/MyMoviePlan/MyMoviePlanApplication.java
@@ -0,0 +1,13 @@
+package com.MyMoviePlan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMoviePlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyMoviePlanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MyMoviePlan/MyMoviePlanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyMoviePlan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMoviePlanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
