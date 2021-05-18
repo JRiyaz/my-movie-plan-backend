88affa555a2228e88ab53e66960807bed0993483
Riyaz J <j.riyazu@gmail.com>
2021-05-18 13:22:28 +0530

    Create UserEntity Class.

---
+++ src/main/java/com/MyMoviePlan/entity/UserEntity.java
@@ -0,0 +1,164 @@
+package com.MyMoviePlan.entity;
+
+import com.MyMoviePlan.model.UserRole;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Table(name = "users")
+public class UserEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    private String id;
+
+    @Column(length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 50, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 10, unique = true)
+    private String mobile;
+
+    @Column(length = 60)
+    private String gender;
+
+    private String password;
+
+    //private String address;
+
+   // @Column(length = 20)
+   // private String city;
+
+   // @Column(length = 20)
+    //private String state;
+
+   // @Column(length = 10)
+    //private String zipcode;
+
+    private Boolean terms;
+
+    @Column(name = "is_account_non_expired")
+    private Boolean isAccountNonExpired;
+
+    @Column(name = "is_account_non_locked")
+    private Boolean isAccountNonLocked;
+
+    @Column(name = "is_credentials_non_expired")
+    private Boolean isCredentialsNonExpired;
+
+    @Column(name = "is_enabled")
+    private Boolean isEnabled;
+
+    @Column(name = "user_role", length = 20)
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+    public UserEntity(String name, String email, String mobile, String gender, String password,
+                      String address, String city, String state, String zipcode, Boolean terms,
+                      Boolean isAccountNonExpired, Boolean isAccountNonLocked,
+                      Boolean isCredentialsNonExpired, Boolean isEnabled, UserRole userRole) {
+        this.name = name;
+        this.email = email;
+        this.mobile = mobile;
+        this.gender = gender;
+        this.password = password;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipcode = zipcode;
+        this.terms = terms;
+        this.isAccountNonExpired = isAccountNonExpired;
+        this.isAccountNonLocked = isAccountNonLocked;
+        this.isCredentialsNonExpired = isCredentialsNonExpired;
+        this.isEnabled = isEnabled;
+        this.userRole = userRole;
+    }
+
+    public UserEntity setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public UserEntity setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public UserEntity setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public UserEntity setMobile(String mobile) {
+        this.mobile = mobile;
+        return this;
+    }
+
+    public UserEntity setGender(String gender) {
+        this.gender = gender;
+        return this;
+    }
+
+    public UserEntity setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public UserEntity setCity(String city) {
+        this.city = city;
+        return this;
+    }
+
+    public UserEntity setState(String state) {
+        this.state = state;
+        return this;
+    }
+
+    public UserEntity setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+        return this;
+    }
+
+    public UserEntity setActive(Boolean active) {
+        terms = active;
+        return this;
+    }
+
+    public UserEntity setAccountNonExpired(Boolean accountNonExpired) {
+        isAccountNonExpired = accountNonExpired;
+        return this;
+    }
+
+    public UserEntity setAccountNonLocked(Boolean accountNonLocked) {
+        isAccountNonLocked = accountNonLocked;
+        return this;
+    }
+
+    public UserEntity setCredentialsNonExpired(Boolean credentialsNonExpired) {
+        isCredentialsNonExpired = credentialsNonExpired;
+        return this;
+    }
+
+    public UserEntity setEnabled(Boolean enabled) {
+        isEnabled = enabled;
+        return this;
+    }
+
+    public UserEntity setUserRole(UserRole userRole) {
+        this.userRole = userRole;
+        return this;
+    }
+}
