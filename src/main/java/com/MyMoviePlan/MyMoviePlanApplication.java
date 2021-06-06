1fff5ce2cfbd114d2eded791f4f72f68c6f0ed34
Riyaz J <j.riyazu@gmail.com>
2021-06-06 11:59:41 +0530

    Update MyMoviePlanApplication and IntitialData classes.

--- src/main/java/com/MyMoviePlan/MyMoviePlanApplication.java
+++ src/main/java/com/MyMoviePlan/MyMoviePlanApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyMoviePlanApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyMoviePlanApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MyMoviePlanApplication.class, args);
+    }
 
 }
