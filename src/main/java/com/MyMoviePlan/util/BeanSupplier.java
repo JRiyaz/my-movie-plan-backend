2302e6e520aa3990a21c6b3dbdaf38c5cef10b99
Riyaz J <j.riyazu@gmail.com>
2021-06-06 11:58:03 +0530

    Update JWTUtil class.

--- src/main/java/com/MyMoviePlan/util/BeanSupplier.java
+++ src/main/java/com/MyMoviePlan/util/BeanSupplier.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class BeanSupplier {
@@ -15,17 +13,18 @@ public class BeanSupplier {
         return new BCryptPasswordEncoder(10);
     }
 
-    @Bean
-    public WebMvcConfigurer crossConfigure() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*")
-                        .allowedHeaders("*")
-                        .allowedOrigins("*")
-                        .allowedMethods("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
+//    @Bean
+//    public FilterRegistrationBean corsFilter() {
+//        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(Boolean.TRUE);
+//        config.addAllowedOrigin(CorsConfiguration.ALL);
+//        config.addAllowedHeader(CorsConfiguration.ALL);
+//        config.addAllowedMethod(CorsConfiguration.ALL);
+//        source.registerCorsConfiguration("/**", config);
+//        FilterRegistrationBean bean = new FilterRegistrationBean();
+//        bean.setFilter(new CorsFilter());
+//        bean.setOrder(0);
+//        return bean;
+//    }
 }
--- src/main/java/com/MyMoviePlan/util/JWTUtil.java
+++ src/main/java/com/MyMoviePlan/util/JWTUtil.java
@@ -24,22 +24,22 @@ public class JWTUtil implements Serializable {
     private String secretKey;
 
     //retrieve username from jwt token
-    public String getUsernameFromToken(String token) {
+    public String getUsernameFromToken(final String token) {
         return getClaimFromToken(token, Claims::getSubject);
     }
 
     //retrieve expiration date from jwt token
-    private Date getExpirationDateFromToken(String token) {
+    private Date getExpirationDateFromToken(final String token) {
         return getClaimFromToken(token, Claims::getExpiration);
     }
 
-    private <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+    private <T> T getClaimFromToken(final String token, final Function<Claims, T> claimsResolver) {
         final Claims claims = getAllClaimsFromToken(token);
         return claimsResolver.apply(claims);
     }
 
     //for retrieving any information from token we will need the secret key
-    private Claims getAllClaimsFromToken(String token) {
+    private Claims getAllClaimsFromToken(final String token) {
         Claims claims = null;
         try {
             claims = Jwts.parser()
@@ -53,24 +53,29 @@ public class JWTUtil implements Serializable {
     }
 
     //check if the token has expired
-    private Boolean isTokenExpired(String token) {
+    private Boolean isTokenExpired(final String token) {
         final Date expiration = getExpirationDateFromToken(token);
         return expiration.before(new Date());
     }
 
     //generate token for user
-    public String generateToken(UserDetails userDetails) {
+//    public String generateToken(UserDetails userDetails) {
+//        Map<String, Object> claims = new HashMap<>();
+//        return doGenerateToken(claims, userDetails.getUsername());
+//    }
+
+    public String generateToken(final String username) {
         Map<String, Object> claims = new HashMap<>();
-        return doGenerateToken(claims, userDetails.getUsername());
+        return doGenerateToken(claims, username);
     }
 
     //while creating the token -
     //1. Define  claims of the token, like Issuer, Expiration, Subject, and the ID
     //2. Sign the JWT using the HS512 algorithm and secret key.
-    private String doGenerateToken(Map<String, Object> claims, String subject) {
+    private String doGenerateToken(final Map<String, Object> claims, final String username) {
         return Jwts.builder()
                 .setClaims(claims)
-                .setSubject(subject)
+                .setSubject(username)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
                 .signWith(SignatureAlgorithm.HS512, secretKey)
@@ -78,12 +83,12 @@ public class JWTUtil implements Serializable {
     }
 
     //validate token
-    public Boolean validateToken(String token, UserDetails userDetails) {
+    public Boolean validateToken(final String token, final UserDetails userDetails) {
         final String username = getUsernameFromToken(token);
         return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
 
-    public String getUserName(String header) {
+    public String getUserName(final String header) {
         return getUsernameFromToken(header.substring(7));
     }
 }
